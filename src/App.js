9ad793759dde502806a42bf53856388b9ab2721f
Abdullah Omar <fishilike.fish@gmail.com>
2020-10-14 03:37:04 +0600

    draggable set up

--- src/App.js
+++ src/App.js
@@ -1,26 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useRef } from 'react';
+import Draggable, { DraggableCore } from 'react-draggable'; // Both at the same time
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	const dragger = useRef('');
+
+	return (
+		<div>
+			<Draggable
+				// axis='x'
+				ref={dragger}
+				handle='.handle'
+				defaultPosition={{ x: 0, y: 0 }}
+				position={null}
+				// grid={[25, 25]}
+				scale={1}
+				// onStart={this.handleStart}
+				// onDrag={this.handleDrag}
+				// onStop={this.handleStop}
+			>
+				<div className='dragger handle'>
+					<div className='handle'>Drag from here</div>
+				</div>
+			</Draggable>
+			<Draggable
+				// axis='x'
+				ref={dragger}
+				handle='.handle'
+				defaultPosition={{ x: 0, y: 0 }}
+				position={null}
+				// grid={[25, 25]}
+				scale={1}
+				// onStart={this.handleStart}
+				// onDrag={this.handleDrag}
+				// onStop={this.handleStop}
+			>
+				<div className='dragger handle'>
+					<div className='handle'>Drag from here</div>
+				</div>
+			</Draggable>
+			<Draggable
+				// axis='x'
+				ref={dragger}
+				handle='.handle'
+				defaultPosition={{ x: 0, y: 0 }}
+				position={null}
+				// grid={[25, 25]}
+				scale={1}
+				// onStart={this.handleStart}
+				// onDrag={this.handleDrag}
+				// onStop={this.handleStop}
+			>
+				<div className='dragger handle'>
+					<div className='handle'>Drag from here</div>
+				</div>
+			</Draggable>
+
+			<div className='btn-container'>
+				<button className='btn'>Align Horizontally</button>
+				<br />
+				<br />
+				<button className='btn'>Align Vertically</button>
+			</div>
+		</div>
+	);
+};
 
 export default App;
